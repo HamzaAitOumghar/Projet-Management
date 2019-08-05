1182d342fa4525765f8827df18c4c1de655dd07e
Hamza Ait Oumghar <hamza.aitoumghar.3@gmail.com>
2019-08-05 16:33:31 +0100

    update project field validation

--- src/main/java/com/projet/service/ProjectServiceImp.java
+++ src/main/java/com/projet/service/ProjectServiceImp.java
@@ -1,6 +1,7 @@
 package com.projet.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.projet.dao.ProjectRepository;
--- src/main/java/com/projet/web/ProjectController.java
+++ src/main/java/com/projet/web/ProjectController.java
@@ -1,11 +1,21 @@
 package com.projet.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projet.dao.ProjectRepository;
 import com.projet.entity.Project;
 import com.projet.service.ProjectService;
 
@@ -14,10 +24,22 @@ import com.projet.service.ProjectService;
 public class ProjectController {
 
 	@Autowired
-	private ProjectService service;
+	private ProjectService projectService;
 	
 	@RequestMapping(value="/add",method = RequestMethod.POST)
-	Project addNewProject(@RequestBody Project p) {
-		return this.service.saveOrUpdateProject(p);
+	ResponseEntity<?> addNewProject(@Valid @RequestBody Project p,BindingResult result) throws Exception
+	{
+		Map<String, String> errorsMessages = new HashMap<String, String>();
+ 
+		for(FieldError error : result.getFieldErrors()) {
+			errorsMessages.put(error.getField(), error.getDefaultMessage());
+		}
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<Map<String, String>>(errorsMessages,HttpStatus.BAD_REQUEST);
+		}
+		
+		Project newProject = this.projectService.saveOrUpdateProject(p);
+		return new ResponseEntity<Project>(newProject,HttpStatus.CREATED);
 	}
 }
